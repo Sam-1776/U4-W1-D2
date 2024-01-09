1227b38294c5e413ee6823376286851cd42ccd75
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-01-09 15:13:51 +0100

    Esercizio 3

---
+++ Esercizio-3/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Esercizio 3");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String x = scanner.nextLine();
+        int i = 0;
+        String punti = ":";
+        String letterQ = "q";
+        String[] newX = new String[0];
+        newX = x.split("");
+        while (i != newX.length){
+            if (Objects.equals(newX[i], punti) || Objects.equals(newX[i], letterQ) ) {
+                break;
+            }
+        System.out.println(newX[i]+ ",");
+        i++;
+        }
+
+    }
+
+}
